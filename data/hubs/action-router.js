eb787711af43d731e55b6d6143a849e416d9d0b0
Josue Rodriguez <jr2589213@gmail.com>
2020-03-13 08:30:31 -0700

    Create routes in order to properly redirect users dynamicaly into the corresponding endpoint hub
    * created hub folder to contain project and action routes
    
    * added basic skeleton structure for crud operations in the both the project and actions router files

---
+++ data/hubs/action-router.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const actionModel = require('../helpers/actionModel')
+
+const action = express.Router()
+
+action.get('/', (req, res) => {
+
+})
+
+action.post('/', (req, res) => {
+
+})
+
+action.put('/:id', (req,res) => {
+
+})
+
+action.put('/:id', (req,res) => {
+    
+})
+
+module.exports = action
---
+++ data/hubs/project-router.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const projectModel = require('../helpers/projectModel')
+
+const project = express.Router()
+
+project.get('/', (req, res) => {
+
+})
+
+project.post('/', (req, res) => {
+
+})
+
+project.put('/:id', (req,res) => {
+
+})
+
+project.put('/:id', (req,res) => {
+    
+})
+
+module.exports = project
